bd2b548c705f649279fad4bc98509a1d1c062e85
Hamish Coleman <hamish@zot.org>
2014-06-08 20:23:52 +0100

    Add filtering and more levels of display

--- lib/CommandLine.pm
+++ lib/CommandLine.pm
@@ -4,6 +4,8 @@ use strict;
 
 use HC::Net::UPnP::ControlPoint;
 
+use URI;
+
 our $VERBOSE = 0;
 my $METHODS = {
     show => \&show,
@@ -27,17 +29,17 @@ sub HANDLE {
     return $METHODS->{$command}($self,$command,@args);
 }
 
-sub show {
-    my $self = shift;
-    my $command = shift;
+#
+sub show_devices {
+    my @devices = @_;
 
-    my @devices = $self->{ControlPoint}->getdevices();
-
+    # group devices by their name
     my $db = {};
     for my $device (@devices) {
         push @{$db->{$device->getfriendlyname()}},$device;
     }
 
+    # find the length of the longest description
     my $maxnamelen =0;
     for my $name (keys %{$db}) {
         if (length($name) > $maxnamelen) {
@@ -61,6 +63,71 @@ sub show {
             }
         }
     }
+}
+
+sub show_services {
+    my @services = @_;
+
+    # arrange by service type
+    my $db;
+    for my $service (@services) {
+        push @{$db->{$service->getservicetype()}},$service;
+    }
+
+    #
+    for my $type (sort keys %{$db}) {
+        printf("  %s\n",$type);
+
+        my @sorted = sort { $a->getserviceid() cmp $b->getserviceid() }
+            @{$db->{$type}};
+        for my $service (@sorted) {
+            printf("    %s\n",$service->getserviceid());
+            if ($VERBOSE) {
+                my $baseurl = URI->new($service->getdevice()->getlocation());
+                my $scpdurl = URI->new($service->getscpdurl());
+                printf("      %s\n",$scpdurl->abs($baseurl));
+            }
+        }
+    }
+}
+
+sub show {
+    my $self = shift;
+    my $command = shift;
+    my $filter_name = shift;
+
+    my @devices = $self->{ControlPoint}->getfiltereddevices($filter_name);
+
+    my $filter_type = shift;
+    if (defined($filter_type)) {
+        @devices = grep {$_->getdevicetype() =~ m/$filter_type/} @devices;
+    }
+
+    show_devices(@devices);
+
+    # if we have more than one device, show no more information
+    if (scalar(@devices) > 1) {
+        return 1;
+    }
+
+    my $filter_servicetype = shift;
+    my @services = $devices[0]->getservicelist();
+
+    if (defined($filter_servicetype)) {
+        @services = grep {
+            $_->getservicetype() =~ m/$filter_servicetype/
+        } @services;
+    }
+
+    my $filter_serviceid = shift;
+    if (defined($filter_serviceid)) {
+        @services = grep {
+            $_->getserviceid() =~ m/$filter_serviceid/
+        } @services;
+    }
+
+
+    show_services(@services);
 }
 
 
